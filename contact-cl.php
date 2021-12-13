5d7744fe233d830f8fd4e92c173380bae4cba8c2
Allysht <81968946+Allysht@users.noreply.github.com>
2021-12-13 22:51:29 +0200

    Add files via upload

---
+++ contact-cl.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="contact.css">
+    <script src="index.js" defer></script>
+    <title>D&K Cleaning | Contact</title>
+</head>
+<body>
+
+    <!--PHP-->
+
+    <?php
+        if(isset( $_POST['name']))
+        $name = $_POST['name'];
+        if(isset( $_POST['email']))
+        $email = $_POST['email'];
+        if(isset( $_POST['message']))
+        $message = $_POST['message'];
+        if(isset( $_POST['subject']))
+        $subject = $_POST['subject'];
+        if ($name === ''){
+        echo "Name cannot be empty.";
+        die();
+        }
+        if ($email === ''){
+        echo "Email cannot be empty.";
+        die();
+        } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "Email format invalid.";
+        die();
+        }
+        }
+        if ($subject === ''){
+        echo "Subject cannot be empty.";
+        die();
+        }
+        if ($message === ''){
+        echo "Message cannot be empty.";
+        die();
+        }
+        $content="From: $name \nEmail: $email \nMessage: $message";
+        $recipient = "youremail@here.com";
+        $mailheader = "From: $email \r\n";
+        mail($recipient, $subject, $content, $mailheader) or die("Error!");
+        echo "Email sent!";
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+header('Content-Type: application/json');
+if ($name === ''){
+print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+exit();
+}
+if ($email === ''){
+print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+exit();
+} else {
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+exit();
+}
+}
+if ($subject === ''){
+print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+exit();
+}
+if ($message === ''){
+print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+exit();
+}
+$content="From: $name \nEmail: $email \nMessage: $message";
+$recipient = "youremail@here.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+exit();
+?>
+
+
+     <!--header-->
+     <div class="header">
+        <img src="dk-logo.png">
+        <ul>
+            <li><a href="dk-cl.html">Acasa</a></li>
+            <li><a href="contact-cl.html">Contact</a></li>
+        </ul>
+    </div>
+
+    <!--Section: Contact v.2-->
+<section class="mb-4">
+
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">Your name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control">
+                            <label for="email" class="">Your email</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-center text-md-left">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>San Francisco, CA 94126, USA</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p>+ 01 234 567 89</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>contact@mdbootstrap.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
+</section>
+<!--Section: Contact v.2-->
+<script>
+    document.getElementById('status').innerHTML = "Sending...";
+formData = {
+'name'     : $('input[name=name]').val(),
+'email'    : $('input[name=email]').val(),
+'subject'  : $('input[name=subject]').val(),
+'message'  : $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+url : "mail.php",
+type: "POST",
+data : formData,
+success: function(data, textStatus, jqXHR)
+{
+
+$('#status').text(data.message);
+if (data.code) //If mail was sent successfully, reset the form.
+$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+},
+error: function (jqXHR, textStatus, errorThrown)
+{
+$('#status').text(jqXHR);
+}
+});
+</script>
+</body>
